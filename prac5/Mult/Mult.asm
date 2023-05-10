c0db6fd8cba76a3411aa2a9717603426f9f226c0
Juddy666 <109936509+Juddy666@users.noreply.github.com>
2023-05-10 16:21:35 +0930

    Add files via upload

--- prac5/Mult/Mult.asm
+++ prac5/Mult/Mult.asm
@@ -7,69 +7,18 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
-@R1
-D=M
-@MULTLOOP
-D;JLE
-@R1
-D=M
-@NEGATE_R1
-D;JLT
-@R3
-M=D
-@MULTLOOP
+@R1 
+D=M //R1
+@R0
+M=0 //result
+
+(LOOP)
+@R2 
+D;JEQ //exit if R2=0
+@R0
+M=M+D // add R1 to result
 @R2
-D=M
-@R4
-M=D
-@R0
-M=0
-(MULTLOOP)
-@R4
-D=M
-@LSB
-D=D&M
-@SKIPADD
-D;JEQ
-@R1
-D=M
-@R0
-D=D+M
-@R0
-M=D
-(SKIPADD)
-@R1
-D=M
-@SHIFT_R1
-M=D
-@R4
-D=M
-@SHIFT_R4
-M=D
-@COUNTER
-MD=M-1
-@MULT_LOOP
-D;JGT
-(END)
-@END
+M=M-1 // Decrement R2
+@LOOP
 0;JMP
-(NEGATE_R1)
-D=0
-D-M
-@R3
-M=D
-@MULT_LOOP
 
-(LSB)
-0000000000000001
-
-(SHIFT_R1)
-D=D>>1
-@MULT_LOOP
-
-(SHIFT_R4)
-D=D>>1
-@MULT_LOOP
-
-(COUNTER)
-0000000000000011
