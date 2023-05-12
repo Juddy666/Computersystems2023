3bc3aaa13867f89ec5b8a00bf4ce6e80ca39a41c
Juddy666 <109936509+Juddy666@users.noreply.github.com>
2023-05-12 14:30:45 +0930

    Add files via upload

--- prac5/ArrMin/ArrMin.asm
+++ prac5/ArrMin/ArrMin.asm
@@ -1,55 +1,87 @@
 // Finds the smallest element in the array of length R2 whose first element is at RAM[R1] and stores the result in R0.
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
+
 // Put your code here.
+@R1
+D=M-1
+@R2
+M=M+D
+
+@32767
+D=A
+@R0
+M=D
+
+(LOOP)
 @R1
 D=M
 @R2
-D=D+M
-
-// Set R0 to maximum value
+D=D-M
+@END
+D;JGT
+@R1
+A=M
+D=M
+@ELEMPOS
+D;JGE
+@ELEMNEG
+(UPDATE)
+@R1
+A=M
+D=M
 @R0
-M=-1
+M=D
+(SKIP)
+@R1
+M=M+1
+@LOOP
+0;JMP
 
-// Loop through array elements
-(loop)
-    // Check if end of array is reached
-    @R2
-    D=M
-    @R1
-    D=D-M
-    @END
-    D;JLE
-
-    // Load element at current position
-    @R1
-    A=M
-    D=M
-
-    // Check if element is smaller than current smallest
-    @R0
-    D=D-M
-    @SMALLER
-    D;JLT
-
-    // Element is not smaller, continue to next element
-    @R1
-    M=M+1
-    @loop
-    0;JMP
-
-(SMALLER)
-    // Update current smallest element
-    @R0
-    M=D
-
-    // Move to next element
-    @R1
-    M=M+1
-    @loop
-    0;JMP
-
-// End of array reached
 (END)
 @END
+0;JMP
+
+(ELEMNEG)
+@R0
+D=M
+@R0NEG
+D;JLT
+@SKIP
+0;JMP
+
+(ELEMPOS)
+@R0
+D=M
+@R0POS
+D;JGE
+@SKIP
+0;JMP
+
+(R0POS)
+@R1
+A=M
+D=M
+@R0
+D=D-M
+@SKIP
+D;JGE
+@UPDATE
+0;JMP
+
+(R0NEG)
+@R1
+A=M
+D=M
+@R0
+D=D-M
+@SKIP
+D;JLT
+@UPDATE
+0;JMP
+
+(SKIP)
+@R1
+M=M+1
+@LOOP
 0;JMP
