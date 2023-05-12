8ec29a1188d6824e84c83b2d2d8d9c52d2010efe
Juddy666 <109936509+Juddy666@users.noreply.github.com>
2023-05-12 15:17:43 +0930

    Add files via upload

---
+++ prac5/ArrMin/Mult.asm
@@ -0,0 +1,57 @@
+
+
+
+// This file is based on part of www.nand2tetris.org
+// and the book "The Elements of Computing Systems"
+// by Nisan and Schocken, MIT Press.
+// File name: Mult.asm
+
+// Multiplies R1 and R2 and stores the result in R0.
+// (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
+
+// Put your code here.
+
+@R0
+M=0
+
+// Jump into the first STEP if R2 > 0.
+@R2
+D=M
+@STEP
+D;JGT
+
+// If not go to END.
+@END
+0;JMP
+
+// Adds R1 to R2 and removes 1 from R0.
+// If R0 is more than 0, loop again.
+(STEP)
+    // Get R0.
+    @R0
+    D=M
+
+    // Add R1.
+    @R1
+    D=D+M
+
+    // write the result back to R2.
+    @R0
+    M=D
+
+    // Reduce R2 by 1.
+    @R2
+    D=M-1
+    M=D
+
+    // If R2 is still > 0 then loop.
+    @STEP
+    D;JGT
+
+(END)
+    @END
+    0;JMP
+
+
+
+
